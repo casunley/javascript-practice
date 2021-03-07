186b9e3a99708462547cd849d96ba9f875312cd5
lauriekallman <disguisedkoala.gmail.com>
2021-03-07 12:52:37 -0500

    Complete forloops lesson

--- scripts/forloops.js
+++ scripts/forloops.js
@@ -1,5 +1,5 @@
 /*
-Display an alert that says "What's it like in New York City" exactly five times,
+Print to the console that says "What's it like in New York City" exactly five times,
 like shown below (each sentence must be on a new line):
 
 What's it like in New York City?
@@ -8,3 +8,7 @@ What's it like in New York City?
 What's it like in New York City?
 What's it like in New York City?
 */
+var c = "What's it like in New York City?";
+    for (n = 0; n < 5; n++){
+        console.log(c);
+    }
